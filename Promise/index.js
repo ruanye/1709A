50f3cab45a0ae62e7b2b9d057437ae58770036c3
ruanye <55343581@qq.com>
2018-11-26 10:10:59 +0800

    node

--- Promise/index.js
+++ Promise/index.js
@@ -4,6 +4,69 @@
   //回调函数里面有2个参数
     // resolve  成功 
     // reject   失败
+  // 没一个promise 都有一个then() 方法 
+  // then方法会返回一个新的promsie(重点)
+  //then 有2个参数 一个成功的回调 一个是失败的回调 
+  // .catch 可以用来统一处理异常
+  // promise 可以链式调用 
+    let p = new Promise((resolve,reject)=>{
+      reject('失败')
+    })
+    p.then(data=>{//data 表示成功的原因
+       console.log(data)
+    }).then(data=>{
+      console.log('resolve')
+    }).catch(e=>{
+      
+    })
+    //async await  配对使用 async 表示函数里面有异步，返回一个promise  await 等待 await后面 通常跟promise 也可以普通值 
+    async function m() {
+       await move(ball1,200);
+       await move(ball2,200);
+       await move(ball3,200);
+    }
+    m().then(data=>{
+      alert('全部走完了')
+    })
+
+    // move(ball1,200).then(data=>{
+    //     return  move(ball2,200)
+    //   }).then(data=>{
+    //     return move(ball3,200)
+    // })
+    // function m(){
+    //   return {name:1}.then()
+    // }
+    // m().then()
+//第一种 
+// move(ball1,200).then(data=>{
+//    move(ball2,200).then(data=>{
+//      move(ball3,200).then(data=>{
+//         alert('我动完了')
+//      })
+//    })
+// })
+ function move(element, target){ 
+          return new Promise((resolve,reject)=>{
+            let m = 0;
+            window.timer = setInterval(() => {
+                m += 5;
+                console.log(m)
+                element.style.left = m + 'px';
+                if (parseInt(element.style.left) > target) {
+                    clearInterval(window.timer)
+                    element.style.left = target + 'px'
+                    resolve('成功')
+                }
+            }, 13)
+          })
+  };
+        
+
+
+
+
+
  // 每一个Promise都有个then的方法，(重点)then()执行后返回一个新的promise  
  //then里面有2个参数（函数） 一个是成功，一个是失败 
  // 如果成功 会走到下个then的成功回调，如果失败会走失败回调 如果返回的是普通值会直接走成功回调     
@@ -11,32 +74,32 @@
 //解决回调地狱的问题
 //  $(this).addClass().css({})
     //return this
- //catch 统一处理异常 
- let p = new Promise((resolve,reject)=>{
-  reject()
-})
-p.then(data=>{ //data 表示成功的原因 
+//  //catch 统一处理异常 
+//  let p = new Promise((resolve,reject)=>{
+//   reject()
+// })
+// p.then(data=>{ //data 表示成功的原因 
  
-}).then(data=>{
-  console.log(data) 
-}).catch(e=>{
-  // console.log(e,'---')
-})
+// }).then(data=>{
+//   console.log(data) 
+// }).catch(e=>{
+//   // console.log(e,'---')
+// })
   
-function move(ele, target){
-  return new Promise((resolve,reject)=>{
-    let m = 0;
-    window.timer = setInterval(() => {
-      m += 5;
-      ele.style.left = m + 'px';
-      if (parseInt(ele.style.left) > target) {
-          clearInterval(window.timer);
-          ele.style.left = target + 'px';
-          resolve('成功')
-      }
-  }, 13)
-  })
-}
+// function move(ele, target){
+//   return new Promise((resolve,reject)=>{
+//     let m = 0;
+//     window.timer = setInterval(() => {
+//       m += 5;
+//       ele.style.left = m + 'px';
+//       if (parseInt(ele.style.left) > target) {
+//           clearInterval(window.timer);
+//           ele.style.left = target + 'px';
+//           resolve('成功')
+//       }
+//   }, 13)
+//   })
+// }
 // move(ball1,200).then(data=>{//第一个成功过移动后会走到then的成功回调 
 //    move(ball2,200).then(data=>{
 //      move(ball3,200).then(data=>{
@@ -65,25 +128,25 @@ function move(ele, target){
 // Promise.all() //接收由promise组成的数组；promise都成功之后才会走then方法 
 //promise.race() 
 // race 跑  谁跑的快结果就是谁 
-let p1 = new Promise((resolve,reject)=>{
-  console.log(1)
-  setTimeout(()=>{
-    resolve('1')
-  },100)
-})
-let p2 = new Promise((resolve,reject)=>{
-  console.log(2)
-  resolve('2')
-})
-//r1 r2 是解构赋值来拿出来的  [r1,r2] = data 
-Promise.all([p1,p2]).then(([r1,r2])=>{
-   console.log(r1,r2,'all')
-})
+// let p1 = new Promise((resolve,reject)=>{
+//   console.log(1)
+//   setTimeout(()=>{
+//     resolve('1')
+//   },100)
+// })
+// let p2 = new Promise((resolve,reject)=>{
+//   console.log(2)
+//   resolve('2')
+// })
+// //r1 r2 是解构赋值来拿出来的  [r1,r2] = data 
+// Promise.all([p1,p2]).then(([r1,r2])=>{
+//    console.log(r1,r2,'all')
+// })
 
-//谁付钱快就收谁的  
-Promise.race([p1,p2]).then((data)=>{
-   console.log(data,'race')
-})
+// //谁付钱快就收谁的  
+// Promise.race([p1,p2]).then((data)=>{
+//    console.log(data,'race')
+// })
 
 
 
--- vue/server.js
+++ vue/server.js
@@ -1,17 +1,47 @@
-let http = require('http');//node的http 模块  node自带的模块都有直接用 
-let  url = require('url');
-//createServer Node创建服务器的方法
-//req request 请求 
-//res response 响应 
-//listen 监听 后面跟着端口号 0--65535
-//(url.parse(req.url,true)) 是一个有很多参数的对象 参数里面包括 query 和pathname 
-//http://localhost:8080/signin?name=1&&age=7
-http.createServer(function(req,res){
-    // query: {},
-    // pathname: '/',
-    //解构赋值  
-    let {query,pathname}= (url.parse(req.url,true));
-    console.log(query,pathname)
-   res.end('8080')
-}).listen(8080)
-// 命令行  node server.js
+//NodeJS  后端 
+let http = require('http');
+//nodejs自带的服务器模块 
+let url = require('url');
+//url  解析url路径和路径参数的  
+//localhost:8080?name=1
+//createServer 创建服务器的函数  
+//req  request  请求 
+//res  response 响应 
+//listen 函数 监听端口号 0- 65535 
+// res.end() 响应回什么数据 
+//pathname 路径名 
+//query  查询参数 ?name=1&&age=2
+//url.parse 解析路径参数的方法 
+//pathname  路径 如果是根路径 是/  其他路径是/ 加上路径的名字 所以我们可以根据路径的不同来响应不同的数据
+// res.setHeader 设置响应头 
+//req.method  请求方式 都是大写的  我们可以根据请求方式的不同来返回不同的数据
+//请求方式有几种  GET POST DELETE PUT OPTIONS(试探性的请求)
+//req.headers  请求头
+http.createServer((req,res)=>{
+   let {pathname,query}= url.parse(req.url,true);
+   res.setHeader('content-type','text/plain;charset=utf8');
+   if(pathname==='/login'){
+      return res.end('登录')
+    }
+   if(pathname==='/sign'){
+     if(req.method==='POST'){
+        //POST请求的处理方式
+        let str = '';
+        req.on('data',chunk=>{
+           str+=chunk;
+        })
+        req.on('end',()=>{
+          console.log(str)
+        })
+       return res.end('POST请求')  
+     }
+     if(req.method==='GET'){
+        return res.end('注册')
+      }else{
+        return res.end('注册成功')
+      }
+    }
+   res.end('not found')
+}).listen(3000,()=>{
+    console.log('3000启动成功')
+})
